f4e7beac95bcd2ad4111edd2c1db736a76b82526
VV <36457838+BezawadaChowdary@users.noreply.github.com>
2019-05-19 13:07:53 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,12 +2,7 @@ variable "cidr_blocks" {
   type        = "list"
   description = "Network CIDR Blocks owned by ADC. Reference: https://wiki.auction.com/pages/viewpage.action?pageId=16883281"
 
-  default = ["206.169.178.0/24",
-    "65.115.111.0/24",
-    "64.58.136.112/28",
-    "128.177.69.0/24",
-    "38.127.82.0/24",
-  ]
+  default = [ ]
 }
 
 variable "from_port" {
